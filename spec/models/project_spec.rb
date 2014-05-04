97e66365e250128053e243cbd0fb3762e24ae7cf
johnh4 <jehowl4@gmail.com>
2014-05-04 08:42:11 -0400

    Added project/ss data. Started view

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Project do
   
+	subject(:project) { FactoryGirl.create(:project) }
+
 	it { should respond_to(:url) }
 	it { should respond_to(:name) }
 	it { should respond_to(:description) }
@@ -12,4 +14,11 @@ describe Project do
 
 	it { should have_many(:screenshots) }
 
+	describe "can have screenshots added" do
+		let(:screenshot) { FactoryGirl.create(:screenshot, project: project) }
+		let(:screenshot_2) { FactoryGirl.create(:screenshot, project: project) }
+
+		its(:screenshots) { should include(screenshot) }
+		its(:screenshots) { should include(screenshot_2) }
+	end
 end
--- spec/models/screenshot_spec.rb
+++ spec/models/screenshot_spec.rb
@@ -2,9 +2,18 @@ require 'spec_helper'
 
 describe Screenshot do
 
+	let(:project) { FactoryGirl.create(:project) }
+	subject(:screenshot) { FactoryGirl.create(:screenshot, project: project) }
+
 	it { should respond_to :filename }
 	it { should respond_to :project }
 
 	it { should belong_to :project }
+	its(:project) { should == project }
+
+	describe "adding a file" do
+		before { screenshot.update(filename: 'new_file') }
+		its(:filename) { should eq('new_file') }
+	end
   
 end
